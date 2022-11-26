e9182daf5c59234a563a96ec9910732aa59591e3
MikeGatt90 <mikegatt76@gmail.com>
2022-11-26 00:51:33 +0000

    code to create EC2 instance via terraform

--- main.tf
+++ main.tf
@@ -1,12 +1,25 @@
-## Providers Example
-provider "azurerm" {
-  version = "3.32.0"
-  features {}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
 }
 
-### AWS
-
 provider "aws" {
-  version = "4.40.0"
   region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = "ami-0b0dcb5067f052a63"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-07f6d680860393307"]
+  subnet_id              = "subnet-072c908573b68f831"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
